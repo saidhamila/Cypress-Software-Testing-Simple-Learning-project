62835fbc5c452512e08bbec1c67af0b11af77ede
bilbini <said@welinxya.com>
2025-10-10 12:40:13 +0100

    Setup decribe('', () => {}) for button tests

--- cypress/e2e/button-tests.cy.js
+++ cypress/e2e/button-tests.cy.js
@@ -2,10 +2,5 @@ describe('Button Tests', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-
-  it('should test all buttons functionality', () => {
-    // Test primary button click counter
-    cy.get('[data-testid="primary-button"]').should('be.visible').click()
-    cy.get('[data-testid="click-count"]').should('contain', 'Clicked: 1 times')
-  })
+  
 })
