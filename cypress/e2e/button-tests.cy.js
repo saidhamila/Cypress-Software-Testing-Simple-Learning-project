19ca98e88b547a8696ea7667a6ab6215f3ad23b4
bilbini <said@welinxya.com>
2025-10-11 01:23:07 +0100

    Add first test: primary button click counter

--- cypress/e2e/button-tests.cy.js
+++ cypress/e2e/button-tests.cy.js
@@ -2,5 +2,13 @@ describe('Button Tests', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-  
+
+  it('should increment click counter when primary button is clicked', () => {
+    // Test primary button click counter
+    cy.get('[data-testid="primary-button"]').should('be.visible').click()
+    cy.get('[data-testid="click-count"]').should('contain', 'Clicked: 1 times')
+    
+    cy.get('[data-testid="primary-button"]').click()
+    cy.get('[data-testid="click-count"]').should('contain', 'Clicked: 2 times')
+  })
 })
